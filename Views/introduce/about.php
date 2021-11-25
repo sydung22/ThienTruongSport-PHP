895ae4a42b7bb9239d51623eab6b44570a7e5399
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-11-25 16:28:46 -0700

    add

---
+++ Views/introduce/about.php
@@ -0,0 +1,221 @@
+<!--[if lt IE 8]>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- pages-title-start -->
+<div class="pages-title section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="pages-title-text text-center">
+					<h2>Giới Thiệu</h2>
+					<ul class="text-left">
+						<li><a href="?act=home">Trang chủ</a></li>
+						<li><span> // </span>Giới Thiệu</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- pages-title-end -->
+<!-- about-us-section-start -->
+<div class="pages about-us section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="main-padding section-padding-top clearfix" style="padding: 70px 10px">
+				<div class="col-sm-12 col-md-6">
+					<div class="about-img">
+						<img src="public/img/about/1.jpg" alt="" />
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6">
+					<div class="about-text">
+						<div class="about-author">
+							<h4>Cửa hàng thể thao Thiên Trường</h4>
+						</div>
+						<h5>Cửa hàng chúng tôi với:</h5>
+						<ol>
+							<li>Chế độ trả góp ưu đãi</li>
+							<li>Thái độ phục vụ tốt</li>
+							<li>Nguồn cung cấp uy tín và ổn định</li>
+							<li>Chế độ bảo hành hợp lý</li>
+						</ol>
+						<p><strong>Thiên Trường Sport</strong> là đơn vị chuyên nhập khẩu, phân phối, bán buôn, bán lẻ dụng cụ thể dục, thể thao và thể hình uy tín nhất tại Việt Nam. Toàn bộ sản phẩm do Thiên Trường cung cấp đều đảm bảo chính hãng, thông tin và giá bán được niêm yết, đảm bảo quyền lợi của Quý khách hàng.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- about-us section end -->
+<!-- team-member section start -->
+<section class="pages team-member section-padding-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="section-title text-center">
+					<h3 style="color:#fff;">Đội ngũ hỗ trợ</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row text-center">
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m1.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Nguyễn Sỹ Dũng</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- single member end -->
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m2.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Đặng Việt Anh</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- single member end -->
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m3.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Võ Duy Việt</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m8.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Nguyễn Văn Trung</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row text-center" style="padding-top:30px">
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m4.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Trần Đình Được</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m5.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Nguyễn Phương Vy</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m6.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Võ Ngọc Trân</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-3">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m7.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Trần Thu Trang</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- team-member section end -->
